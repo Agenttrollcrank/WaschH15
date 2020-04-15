e65d654b2109a59805df950f78af65a0b26a8ebe
Agenttrollcrank <63065954+Agenttrollcrank@users.noreply.github.com>
2020-04-15 15:43:59 +0200

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -8,14 +8,8 @@ mydb = mysql.connector.connect(
     host='127.0.0.1',
     database='sys')
 
-mycursor = mydb.cursor()
+mycursor = mydb.cursor(buffered=True)
 
-mycursor.execute("SELECT * FROM strom")
-
-myresult = mycursor.fetchall()
-
-for row in myresult:
-    print(myresult)
 
 #define the windown
 root = Tk()
@@ -23,31 +17,57 @@ root.title("WaschH15")
 root.geometry("500x500")
 
 #variable definitions
-electricityOldValue = 0
+electricityOldValue = 0 #defined because it needs to be public
+sl = 0
 
 #define the function for buttons
 def myClick(): #excecute button
-    selection = r.get()
-    print(selection)
+    #credential check
+    mycursor.execute("SELECT COUNT(1) FROM sys.benutzer WHERE username='%s'" % (usernameIN.get()))
+    if mycursor.fetchone()[0]:
+        mycursor.execute("SELECT COUNT(1) FROM sys.benutzer WHERE password='%s'" % (passwordIN.get()))
+        if mycursor.fetchone()[0]:
+            try: #checks the value inputted
+                if int(electricityNewBox.get()) >= electricityOldValue:
+                    mycursor.execute(
+                        "UPDATE sys.strom SET Kwh = " + str(electricityNewBox.get()) + " WHERE Waschmachine = '" + str(
+                            machineString.get()) + "'")
+                else:
+                    Label(root, text="Bitte geben Sie einen größeren Wert ein").grid(row=14, column=9)
+            except:  # ValueError
+                Label(root, text="Bitte geben Sie eine Zahl ein").grid(row=14, column=9)
+            electricityOldBox.delete(0, END)
+            electricityNewBox.delete(0, END)
+            usernameIN.delete(0, END)
+            passwordIN.delete(0, END)
+        else:
+            Label(root, text="Falsche Password").grid(row=14, column=9)
+    else:
+        Label(root, text="Unbekante Benutzername").grid(row=14, column=9)
+    mydb.commit()
+    #TODO add new entry
+    #TODO take the new electricity value from the database
 
-def newSelection(t): #machine choice buttons, changes the text and previous value TODO: change the values to the ones from the database
-    machineSelection.config(text=t)
-    if t == "Altbau":
-        electricityOldValue = 1
-    elif t == "Linke Maschine":
-        electricityOldValue = 2
-    elif t == "Mitlere Maschine":
-        electricityOldValue = 3
-    elif t == "Rechte Maschine":
-        electricityOldValue = 4
-    elif t == "Trockner Oben":
-        electricityOldValue = 5
-    elif t == "Trockner Unten":
-        electricityOldValue = 6
+def newSelection(): #machine choice buttons, changes the text and previous value
+    machineSelection.config(text=machineString.get())
+    if machineString.get() == "Altbau":
+        sl = 0
+    elif machineString.get() == "Linke_Maschine":
+        sl = 1
+    elif machineString.get() == "Mittlere_Maschine":
+        sl = 2
+    elif machineString.get() == "Rechte_Maschine":
+        sl = 3
+    elif machineString.get() == "Trockner_Oben":
+        sl = 4
+    elif machineString.get() == "Trockner_Unten":
+        sl = 5
     else:
         exit(1)
+    mycursor.execute("SELECT Kwh FROM sys.strom LIMIT " + str(sl) +",1")
+    electricityOldValue = mycursor.fetchall()
     electricityOldBox.delete(0, END)
-    electricityOldBox.insert(0, str(electricityOldValue))
+    electricityOldBox.insert(0, electricityOldValue)
 
 
 titleLabel = Label(root, text="WaschH15")
@@ -56,31 +76,31 @@ machineSelection = Label(root, text="")
 usernameIN = Entry(root, width=40)
 passwordIN = Entry(root, width=40)
 electricityOldBox = Entry(root, width=40)
-electricityOldBox.insert(0,str(electricityOldValue))
+electricityOldBox.insert(0,0)
 electricityNewBox = Entry(root, width=40)
 
 login = Button(root, text="Login", command=myClick, bg="#ff00ff")
-
+#defining the radio buttons
 MACHINES = [
     ("Altbau", "Altbau"),
-    ("Linke Maschine", "Linke Maschine"),
-    ("Mitlere Maschine", "Mitlere Maschine"),
-    ("Rechte Maschine", "Rechte Maschine"),
-    ("Trockner Oben", "Trockner Oben"),
-    ("Trockner Unten", "Trockner Unten")
+    ("Linke_Maschine", "Linke_Maschine"),
+    ("Mittlere_Maschine", "Mittlere_Maschine"),
+    ("Rechte_Maschine", "Rechte_Maschine"),
+    ("Trockner_Oben", "Trockner_Oben"),
+    ("Trockner_Unten", "Trockner_Unten")
 ]
-
-r = StringVar()
+#drawing the radio buttons on the screen
+machineString = StringVar()
 line = 3
 for text, machine in MACHINES:
-    Radiobutton(root, text=text, variable=r, value=machine, tristatevalue=0, command=lambda: newSelection(r.get())).grid(row=line, column=10)
+    Radiobutton(root, text=text, variable=machineString, value=machine, tristatevalue=0, command=lambda: newSelection()).grid(row=line, column=10)
     line += 1
 
 #showing it on screen
 titleLabel.grid(row=0, column=10)
-username = Label(root, text="Benutzername: ").grid(row=1, column=9)
+usernameLabel = Label(root, text="Benutzername: ").grid(row=1, column=9)
 usernameIN.grid(row=1, column=10)
-password = Label(root, text="Passwort: ").grid(row=2, column=9)
+passwordLabel = Label(root, text="Passwort: ").grid(row=2, column=9)
 passwordIN.grid(row=2, column=10)
 
 #machineSelection.grid(row=10, column=10)
@@ -89,6 +109,5 @@ electricityOldBox.grid(row=11, column=10)
 electricityNewLabel = Label(root, text="Neuer Strom Stand: ").grid(row=12, column=9)
 electricityNewBox.grid(row=12, column=10)
 login.grid(row=13, column=10)
-
 
 root.mainloop()
