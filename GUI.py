ea2f035a6861c7e7c3d7817a8e8d136cac53c729
Agenttrollcrank <63065954+Agenttrollcrank@users.noreply.github.com>
2020-04-17 02:05:21 +0200

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -13,7 +13,11 @@ mycursor = mydb.cursor(buffered=True)
 #define the windown
 root = Tk()
 root.title("WaschH15")
-root.geometry("500x500")
+root.geometry("1920x1080")
+
+Wasch = LabelFrame(root, padx=5,pady=5)
+Table = LabelFrame(root, padx=5,pady=5)
+
 
 #variable definitions
 sl = 0
@@ -27,16 +31,19 @@ def myClick(): #excecute button
         mycursor.execute("SELECT COUNT(1) FROM h15.benutzer WHERE passwort='%s' AND username='%s'" % (passwordIN.get(),usernameIN.get()))
         if mycursor.fetchone()[0]:
             if int(electricityNewBox.get()) >= electricityOldValue[0]:
-                try: #checks the value inputted
+                try:#checks the value inputted
                     mycursor.execute("UPDATE h15.strom SET Kwh = " + str(electricityNewBox.get()) +
                                      " WHERE Waschmachine = '" + str(machineString.get()) + "'")
-                    message.config(text="Alles Klar! Danke dir! Vergesse nicht deine Wäsche :D")
+                    mycursor.execute("INSERT INTO abrechnung VALUES('%s','%s',%s,%s)" % (
+                    str(usernameIN.get()), str(machineString.get()), str(electricityOldValue[0]),
+                    str(electricityNewBox.get())))
+                    message.config(text="Alles Klar! Danke dir! :D")
                     electricityOldBox.delete(0, END)
                     electricityOldBox.insert(0, electricityNewBox.get())
                     usernameIN.delete(0, END)
                     passwordIN.delete(0, END)
                 except:
-                    message.config(text="Bitte geben Sie einen zahl an")
+                    message.config(text="Irgendein Error ist aufgetreten, sorry...")
             else:  # ValueError
                 message.config(text="Bitte geben Sie einen größeren Wert ein")
             electricityNewBox.delete(0, END)
@@ -45,75 +52,121 @@ def myClick(): #excecute button
     else:
         message.config(text="Falsche Benutzername")
     mydb.commit()
-    #TODO add new entry
+    tableUpdate(str(machineString.get()))
+def tableUpdate(mch):
+    headings = ["Name","Strom Von","Strom Bist"]
+    values = ["username","strom_von","strom_bist"]
+    for row in range(5):
+        if row == 0:
+            for column in range(3):
+                label = Label(Table, text=headings[column], bg="white", fg="black", padx=30, pady=3)
+                label.grid(row=row,column=(column),sticky="nsew",padx=1,pady=1)
+                label.config(font=('Arial', 18, "bold"))
+                Table.grid_columnconfigure((column),weight=1)
+        else:
+            for column in range(3):
+                mycursor.execute("SELECT %s FROM h15.abrechnung WHERE MATCH(machine) AGAINST('%s') LIMIT %s,1" % (str(values[column]), mch, (row-1)))
+                label = Label(Table, text=mycursor.fetchone(), bg="white", fg="black", padx=30, pady=3)
+                label.grid(row=row, column=column, sticky="nsew", padx=1, pady=1)
+                label.config(font=('Arial', 18))
+                Table.grid_columnconfigure(column, weight=1)
 
+
+
+titleLabel = Label(root, text="WaschH15")
+usernameLabel = Label(Wasch, text="Benutzername: ")
+passwordLabel = Label(Wasch, text="Passwort: ")
+machineSelection = Label(Wasch, text="")
+usernameIN = Entry(Wasch, width=25)
+passwordIN = Entry(Wasch, show="*", width=25)
+electricityOldBox = Entry(Wasch, width=25)
+electricityOldBox.insert(0,0)
+electricityNewBox = Entry(Wasch, width=25)
+login = Button(Wasch, text="WASCHEN!!!", command=myClick)
+
+#defining the radio buttons
 def newSelection(): #machine choice buttons, changes the text and previous value
     global electricityOldValue
     machineSelection.config(text=machineString.get())
     if machineString.get() == "Altbau":
         sl = 0
+        tableUpdate("Altbau")
     elif machineString.get() == "Linke_Maschine":
         sl = 1
+        tableUpdate("Linke_Maschine")
     elif machineString.get() == "Mittlere_Maschine":
         sl = 2
+        tableUpdate("Mittlere_Maschine")
     elif machineString.get() == "Rechte_Maschine":
         sl = 3
+        tableUpdate("Rechte_Maschine")
     elif machineString.get() == "Trockner_Oben":
         sl = 4
+        tableUpdate("Trockner_Oben")
     elif machineString.get() == "Trockner_Unten":
         sl = 5
+        tableUpdate("Trockner_Unten")
     else:
         exit(1)
     mycursor.execute("SELECT Kwh FROM h15.strom LIMIT " + str(sl) +",1")
     electricityOldValue = mycursor.fetchone()
     electricityOldBox.delete(0, END)
     electricityOldBox.insert(0, electricityOldValue)
-
-titleLabel = Label(root, text="WaschH15")
-machineSelection = Label(root, text="")
-
-usernameIN = Entry(root, width=40)
-passwordIN = Entry(root, show="*", width=40)
-electricityOldBox = Entry(root, width=40)
-electricityOldBox.insert(0,0)
-electricityNewBox = Entry(root, width=40)
-
-login = Button(root, text="WASCHEN!!!", command=myClick)
-#defining the radio buttons
 MACHINES = [
     ("Altbau", "Altbau"),
-    ("Linke_Maschine", "Linke_Maschine"),
-    ("Mittlere_Maschine", "Mittlere_Maschine"),
-    ("Rechte_Maschine", "Rechte_Maschine"),
-    ("Trockner_Oben", "Trockner_Oben"),
-    ("Trockner_Unten", "Trockner_Unten")
+    ("Linke Maschine", "Linke_Maschine"),
+    ("Mittlere Maschine", "Mittlere_Maschine"),
+    ("Rechte Maschine", "Rechte_Maschine"),
+    ("Trockner Oben", "Trockner_Oben"),
+    ("Trockner Unten", "Trockner_Unten")
 ]
 #drawing the radio buttons on the screen
 machineString = StringVar()
 line = 3
 for text, machine in MACHINES:
-    Radiobutton(root, text=text, variable=machineString, value=machine, tristatevalue=0, command=lambda: newSelection()).grid(row=line, column=10)
+    button = Radiobutton(Wasch, text=text, variable=machineString, value=machine, tristatevalue=0, command=lambda: newSelection())
+    button.grid(row=line, column=1, sticky="w")
+    button.config(font=('Arial', 18))
     line += 1
 
 #showing it on screen
-titleLabel.grid(row=0, column=10)
-usernameLabel = Label(root, text="Benutzername: ").grid(row=1, column=9)
-usernameIN.grid(row=1, column=10)
-passwordLabel = Label(root, text="Passwort: ").grid(row=2, column=9)
-passwordIN.grid(row=2, column=10)
+titleLabel.pack()
+titleLabel.config(font=('Arial', 24))
+Wasch.pack()
+Table.pack()
 
-#machineSelection.grid(row=10, column=10)
-electricityOldLabel = Label(root, text="Vorheriger Strom Stand: ").grid(row=11, column=9)
-electricityOldBox.grid(row=11, column=10)
-electricityNewLabel = Label(root, text="Neuer Strom Stand: ").grid(row=12, column=9)
-electricityNewBox.grid(row=12, column=10)
-login.grid(row=13, column=10)
-message = Label(root, text="")
-message.grid(row=14, column=10)
+usernameLabel.grid(row=1, column=0)
+usernameLabel.config(font=('Arial', 18))
+usernameIN.grid(row=1, column=1)
+usernameIN.config(font=('Arial', 18))
 
-#TODO display last user behind the machine
-#TODO radio button from left to right
+passwordLabel.grid(row=2, column=0)
+passwordLabel.config(font=('Arial', 18))
+passwordIN.grid(row=2, column=1)
+passwordIN.config(font=('Arial', 18))
+
+electricityOldLabel = Label(Wasch, text="Vorheriger Strom Stand: ")
+electricityOldLabel.grid(row=11, column=0)
+electricityOldLabel.config(font=('Arial', 18))
+
+electricityOldBox.grid(row=11, column=1)
+electricityOldBox.config(font=('Arial', 18))
+
+electricityNewLabel = Label(Wasch, text="Neuer Strom Stand: ")
+electricityNewLabel.grid(row=12, column=0)
+electricityNewLabel.config(font=('Arial', 18))
+
+electricityNewBox.grid(row=12, column=1)
+electricityNewBox.config(font=('Arial', 18))
+
+login.grid(row=13, column=1,sticky="w")
+login.config(font=('Arial', 18))
+
+message = Label(Wasch, text="")
+message.config(font=('Arial', 18))
+message.grid(row=14, column=0)
+
+tableUpdate("Altbau")
+
 #TODO graphical shit, Logo, Big label,
-#TODO Table for feedback
-
 root.mainloop()
